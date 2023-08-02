dca01ce1e713d3a4f56ac92a2708f84fb0cc43b0
Raja Faizan Nazir <RFaizanN@gmail.com>
2023-08-02 23:48:24 +0200

    Update server.js

--- server.js
+++ server.js
@@ -10,10 +10,23 @@ var clickCount = 0;
 io.on("connection", function (client) {
   //when the server receives clicked message, do this
   client.on("status", function (data) {
-    for (i = 0; i < 10; i++) {
+    let i = 0;
+    let totalMessage = 50000;
+    for (; i < totalMessage; i++) {
       clickCount++;
-      io.emit(data.fileId, clickCount);
+      io.emit(data.fileId, clickCount, (delivered, callback) => {
+        if (delivered) {
+          // no ack from the server, let's retry
+          // emit(socket, event, arg);
+          //   console.log("delivered", clickCount);
+        } else {
+          console.log("not delivered", clickCount);
+          totalMessage = i;
+        }
+      });
     }
+    console.log(i, " message delivered");
+    console.log("End of status");
   });
   console.log("End of connection");
 });
